0b7da0febecd5f0366f522474536a09559c1309d
manuelaraoz <manuelaraoz@e02e0c50-9a77-148c-8b03-882bfb837882>
2010-09-29 21:02:37 +0000

    fin del script

---
+++ 2tp/scripts/code.m
@@ -0,0 +1,51 @@
+function answer = code()
+	im = load('../saturno');
+	colormap(gray(255));
+	
+	image(im');
+	t = fftn(im);
+	%it = ifftn(t);
+
+	% abs to remove small imaginary part (machine error)
+	%image(abs(it)');
+
+	%ampl = abs(t);
+	%mx = max(max(ampl));
+	%mn = min(min(ampl));
+
+	%m = 255/(mx-mn);
+	%b = -m*mn;
+
+	%nampl = floor(m*ampl + b);
+	%image(nampl');
+
+	%phase = angle(t);
+	%mx = max(max(phase));
+    %mn = min(min(phase));
+
+	%m = 255/(mx-mn);
+    %b = -m*mn;
+
+    %nphase = floor(m*phase + b)
+	%image(nphase');
+
+	%pul = pulse()
+	%f2 = t.*pul;
+	%it2 = abs(ifftn(f2))';
+	%image(it2);
+
+	ga = gaussiano();
+	f3 = t.*ga;
+	it3 = abs(ifftn(f3))';
+	image(it3);	
+
+	%dam = damero();
+	%f1 = t.*dam;
+	%it1 = abs(ifftn(f1))';
+	%image(it1);
+
+	
+
+
+
+end
