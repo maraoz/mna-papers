2605200e442460c64da4a30628fcb9aa9f08a88b
williams.matias <williams.matias@e02e0c50-9a77-148c-8b03-882bfb837882>
2010-09-29 20:08:09 +0000

    bug fix

--- 2tp/scripts/damero.m
+++ 2tp/scripts/damero.m
@@ -2,7 +2,7 @@ function [matrix] = damero()
     matrix = ones(400,400);
 	for k = 1:1:400
         for l = 1:1:400
-            if  l+k%2 == 0
+            if  mod(l+k,2) == 0
                 matrix(k,l) = 0;
             end
         end
